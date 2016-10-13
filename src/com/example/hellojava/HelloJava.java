21fdf70bdca1cbc942c902df2fa8bb209226decd
Heather Hang Tran <heatherhangtran@Heathers-MacBook-Pro.local>
2016-10-12 17:44:22 -0700

    Adding java project for Jenkins.

---
+++ src/com/example/hellojava/HelloJava.java
@@ -0,0 +1,10 @@
+package com.example.hellojava;
+
+/**
+ * Created by heatherhangtran on 10/12/16.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java.");
+    }
+}
